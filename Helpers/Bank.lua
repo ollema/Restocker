8ecabdfa0a832b95ec49defff1539796dfccf7a4
RainForDays <sebb.tornquist@gmail.com>
2020-05-11 23:26:36 +0200

    Reenabled new banking

--- Helpers/Bank.lua
+++ Helpers/Bank.lua
@@ -1,233 +1,295 @@
---[[
+local _, core = ...;
 
-  local _, core = ...;
-  
-  core.didBankStuff = false
-  local containerFreeSlots = {}
-  core.justSplit = false
-  core.splitLoc = {}
-  
-  local bankBags = {-1,5,6,7,8,9,10}
-  local bankBagsReversed = {10,9,8,7,6,5,-1}
-  
-  local function count(T)
-    local i = 0
-    for _,_ in pairs(T) do
-      i = i+1
-    end
-    return i
+core.didBankStuff = false
+local containerFreeSlots = {}
+core.justSplit = false
+core.splitLoc = {}
+
+local bankBags = {-1,5,6,7,8,9,10}
+local bankBagsReversed = {10,9,8,7,6,5,-1}
+
+local function count(T)
+  local i = 0
+  for _,_ in pairs(T) do
+    i = i+1
   end
-  
-  
-  local function IsItemInRestockList(item)
-    local type = ""
-    if tonumber(item) then
-      type = "itemID"
-    elseif string.find(item, "Hitem:") then
-      type = "itemLink"
-    else
-      type = "itemName"
-    end
-  
-    for _, restockItem in ipairs(Restocker.profiles[Restocker.currentProfile]) do
-      if restockItem[type] == item then
+  return i
+end
+
+
+local function somethingLocked()
+  for bag = BACKPACK_CONTAINER, NUM_BAG_SLOTS do
+    for slot = 1, GetContainerNumSlots(bag) do
+      local _, _, locked = GetContainerItemInfo(bag, slot)
+      if locked then
         return true
       end
     end
-    return false
   end
-  
-  
-  local function GetRestockItemIndex(item)
-    local type = ""
-    if tonumber(item) then
-      type = "itemID"
-    elseif string.find(item, "Hitem:") then
-      type = "itemLink"
-    else
-      type = "itemName"
-    end
-  
-    for i, restockItem in ipairs(Restocker.profiles[Restocker.currentProfile]) do
-      if restockItem[type] == item then
-        return i
+
+  for _,bag in ipairs(bankBagsReversed) do
+    for slot = 1, GetContainerNumSlots(bag) do
+      local _, _, locked = GetContainerItemInfo(bag, slot)
+      if locked then
+        return true
       end
     end
-    return nil
   end
+
+  return false
+end
+
+
+local function IsItemInRestockList(item)
+  local type = ""
+  if tonumber(item) then
+    type = "itemID"
+  elseif string.find(item, "Hitem:") then
+    type = "itemLink"
+  else
+    type = "itemName"
+  end
+
+  for _, restockItem in ipairs(Restocker.profiles[Restocker.currentProfile]) do
+    if restockItem[type] == item then
+      return true
+    end
+  end
+  return false
+end
+
+
+local function GetRestockItemIndex(item)
+  local type = ""
+  if tonumber(item) then
+    type = "itemID"
+  elseif string.find(item, "Hitem:") then
+    type = "itemLink"
+  else
+    type = "itemName"
+  end
+
+  for i, restockItem in ipairs(Restocker.profiles[Restocker.currentProfile]) do
+    if restockItem[type] == item then
+      return i
+    end
+  end
+  return nil
+end
+
+local function GetItemsInBags()
+  local T = {}
+  for bag = BACKPACK_CONTAINER, NUM_BAG_SLOTS do
+    for slot = 1, GetContainerNumSlots(bag) do
+      local _, itemCount, locked, _, _, _, itemLink, _, _, itemID = GetContainerItemInfo(bag, slot)
+      if itemID then
+        local itemName = GetItemInfo(itemID)
+
+        T[itemName] = T[itemName] and T[itemName] + itemCount or itemCount
+      end
+    end
+  end
+  return T
+end
+
+
+local function GetFreeBagSlots()
+  local T = {}
+  for bag = BACKPACK_CONTAINER, NUM_BAG_SLOTS do
+    for slot = 1, GetContainerNumSlots(bag) do
+      local _, itemCount, locked, _, _, _, itemLink, _, _, itemID = GetContainerItemInfo(bag, slot)
+      if not itemID then
+        local slot = {}
+        slot.bag = bag
+        slot.slot = slot
+        tinsert(T, slot)
+      end
+    end
+  end
+  return T
+end
+
+
+
+local function PutSplitItemIntoBags(item, amountOnMouse)
+  if not CursorHasItem() then return end
+  C_NewItems.ClearAll()
   
-  local function GetItemsInBags()
-    local T = {}
-    for bag = 0, NUM_BAG_SLOTS do
-      for slot = 1, GetContainerNumSlots(bag) do
-        local _, itemCount, locked, _, _, _, itemLink, _, _, itemID = GetContainerItemInfo(bag, slot)
-        if itemID then
-          local itemName = GetItemInfo(itemID)
-  
-          T[itemName] = T[itemName] and T[itemName] + itemCount or itemCount
+  for bag = BACKPACK_CONTAINER, NUM_BAG_SLOTS do
+    for slot = 1, GetContainerNumSlots(bag) do
+      local _, itemCount, locked, _, _, _, _, _, _, itemID = GetContainerItemInfo(bag, slot)
+      if itemID and not locked then
+        local itemName, _, _, _, _, _, _, itemStackCount = GetItemInfo(itemID)
+        if itemName == item.itemName and itemCount+amountOnMouse <= itemStackCount then
+          PickupContainerItem(bag, slot)
         end
       end
     end
-    return T
   end
   
-  
-  
-  local function PutSplitItemIntoBags(item, amountOnMouse)
-    if not CursorHasItem() then return end
-    C_NewItems.ClearAll()
-  
-    for bag = 0, NUM_BAG_SLOTS do
-      for slot = 1, GetContainerNumSlots(bag) do
-        local _, itemCount, locked, _, _, _, _, _, _, itemID = GetContainerItemInfo(bag, slot)
-        if itemID and not locked then
-          local itemName, _, _, _, _, _, _, itemStackCount = GetItemInfo(itemID)
-          if itemName == item.itemName and itemCount+amountOnMouse <= itemStackCount then
-            PickupContainerItem(bag, slot)
+
+  for bag = BACKPACK_CONTAINER, NUM_BAG_SLOTS do
+    local numberOfFreeSlots, bagType = GetContainerNumFreeSlots(bag)
+    if numberOfFreeSlots > 0 then
+      local currentBag = bag+19
+      if currentBag == 19 then
+        PutItemInBackpack()
+        return
+      else
+        PutItemInBag(currentBag)
+        return
+      end
+    end
+  end
+end
+
+
+
+local function bankTransfer()
+  local itemsInBags = GetItemsInBags()
+  local freeSlots = GetFreeBagSlots()
+
+  local currentProfile = Restocker.profiles[Restocker.currentProfile]
+  local rightClickedItem = false
+  local hasSplitItems = false
+  local transferredToBank = false
+
+  --
+  --  INVENTORY
+  --
+  for bag = NUM_BAG_SLOTS, 0, -1 do
+    for slot = GetContainerNumSlots(bag), 1, -1 do
+      local _, itemCount, locked, _, _, _, itemLink, _, _, itemID = GetContainerItemInfo(bag, slot)
+      if itemID then
+        local inRestockList = IsItemInRestockList(itemID)
+
+        if not locked and inRestockList then
+          local item = currentProfile[GetRestockItemIndex(itemID)]
+          local numInBags = itemsInBags[item.itemName] or 0
+          --local numInBank = GetItemCount(itemLink, true) - numInBags
+          local restockNum = item.amount
+          local difference = restockNum-numInBags
+
+
+          if difference < 0 then
+            UseContainerItem(bag, slot)
+            itemsInBags[item.itemName] = itemsInBags[item.itemName] and itemsInBags[item.itemName] - itemCount
+            rightClickedItem = true
+            transferredToBank = true
+            --coroutine.yield()
           end
         end
-      end
-    end
-  
-    for bag = 0, NUM_BAG_SLOTS do
-      local numberOfFreeSlots, bagType = GetContainerNumFreeSlots(bag)
-      if numberOfFreeSlots > 0 then
-        local currentBag = bag+19
-        if currentBag == 19 then
-          PutItemInBackpack()
-          return
-        else
-          PutItemInBag(currentBag)
-          return
-        end
-      end
-    end
-  end
-  
-  
-  
-  local function bankTransfer()
-    local itemsInBags = GetItemsInBags()
-  
-    local currentProfile = Restocker.profiles[Restocker.currentProfile]
-    local rightClickedItem = false
-    local hasSplitItems = false
-    local transferredToBank = false
-  
-    --
-    --  INVENTORY
-    --
-    for bag = NUM_BAG_SLOTS, 0, -1 do
+      end -- if item we should get and its not locked
+    end -- for slot
+  end -- for bag
+
+
+
+
+  --
+  --  BANK
+  --
+  if not transferredToBank then
+    for _, bag in ipairs(bankBagsReversed) do
       for slot = GetContainerNumSlots(bag), 1, -1 do
         local _, itemCount, locked, _, _, _, itemLink, _, _, itemID = GetContainerItemInfo(bag, slot)
-        if itemID then
+        if itemID and not locked then
           local inRestockList = IsItemInRestockList(itemID)
-  
+
           if not locked and inRestockList then
             local item = currentProfile[GetRestockItemIndex(itemID)]
             local numInBags = itemsInBags[item.itemName] or 0
             --local numInBank = GetItemCount(itemLink, true) - numInBags
             local restockNum = item.amount
             local difference = restockNum-numInBags
-  
-  
-            if difference < 0 then
+
+
+            if difference > 0 and itemCount <= difference then
               UseContainerItem(bag, slot)
-              itemsInBags[item.itemName] = itemsInBags[item.itemName] and itemsInBags[item.itemName] - itemCount
+              itemsInBags[item.itemName] = itemsInBags[item.itemName] and itemsInBags[item.itemName] + itemCount or itemCount
               rightClickedItem = true
-              transferredToBank = true
+              --coroutine.yield()
             end
           end
         end -- if item we should get and its not locked
       end -- for slot
     end -- for bag
-  
-  
-  
-  
-    --
-    --  BANK
-    --
-    if not transferredToBank then
-      for _, bag in ipairs(bankBagsReversed) do
-        for slot = GetContainerNumSlots(bag), 1, -1 do
-          local _, itemCount, locked, _, _, _, itemLink, _, _, itemID = GetContainerItemInfo(bag, slot)
-          if itemID and not locked then
-            local inRestockList = IsItemInRestockList(itemID)
-  
-            if not locked and inRestockList then
-              local item = currentProfile[GetRestockItemIndex(itemID)]
-              local numInBags = itemsInBags[item.itemName] or 0
-              --local numInBank = GetItemCount(itemLink, true) - numInBags
-              local restockNum = item.amount
-              local difference = restockNum-numInBags
-  
-  
-              if difference > 0 and itemCount <= difference then
-                UseContainerItem(bag, slot)
-                itemsInBags[item.itemName] = itemsInBags[item.itemName] and itemsInBags[item.itemName] + itemCount or itemCount
-                rightClickedItem = true
-              end
-            end
-          end -- if item we should get and its not locked
-        end -- for slot
-      end -- for bag
-    end
-  
-  
-  
-    if not rightClickedItem then
-      for _, bag in ipairs(bankBagsReversed) do
-        for slot = GetContainerNumSlots(bag), 1, -1 do
-          local _, itemCount, locked, _, _, _, itemLink, _, _, itemID = GetContainerItemInfo(bag, slot)
-          if itemID and not locked then
-            local inRestockList = IsItemInRestockList(itemID)
-  
-            if not locked and inRestockList then
-              local item = currentProfile[GetRestockItemIndex(itemID)]
-              local numInBags = itemsInBags[item.itemName] or 0
-              --local numInBank = GetItemCount(itemLink, true) - numInBags
-              local restockNum = item.amount
-              local difference = restockNum-numInBags
-  
-              if difference > 0 and itemCount > difference then
-                SplitContainerItem(bag, slot, difference)
-                coroutine.yield()
-                PutSplitItemIntoBags(item, difference)
-  
-                itemsInBags[item.itemName] = itemsInBags[item.itemName] and itemsInBags[item.itemName] + difference or difference
-                hasSplitItems = true
-                coroutine.yield()
-              end
-            end
-          end -- if item we should get and its not locked
-        end -- for slot
-      end -- for bag
-    end -- not right clicked an item
-  
-  
-    -- and hasSplitItems == false 
-    if rightClickedItem == false and transferredToBank == false then
-      core.currentlyRestocking = false
-      core:Print(core.defaults.prefix .. "finished restocking from bank.")
-    end
   end
-  
-  
-  restockerCoroutine = coroutine.create(bankTransfer)
-  
-  
-  --
-  -- OnUpdate frame
-  --
-  
-  local onUpdateFrame = CreateFrame("Frame")
-  local ONUPDATE_INTERVAL = 0.2
-  local timer = 0
-  
-  onUpdateFrame:SetScript("OnUpdate", function(self, elapsed)
-    timer = timer+elapsed
-    if core.currentlyRestocking then
+
+
+
+  if not rightClickedItem then
+    for _, bag in ipairs(bankBagsReversed) do
+      for slot = GetContainerNumSlots(bag), 1, -1 do
+        local _, itemCount, locked, _, _, _, itemLink, _, _, itemID = GetContainerItemInfo(bag, slot)
+
+        if itemID and not locked then
+          local inRestockList = IsItemInRestockList(itemID)
+          local itemStackSize = select(8, GetItemInfo(itemLink))
+          
+
+          if inRestockList then
+            local item = currentProfile[GetRestockItemIndex(itemID)]
+            local numInBags = itemsInBags[item.itemName] or 0
+            --local numInBank = GetItemCount(itemLink, true) - numInBags
+            local restockNum = item.amount
+            local difference = restockNum-numInBags
+
+            if difference > 0 and itemCount > difference then
+              if mod(difference+numInBags, itemStackSize) == 0 then
+                PickupContainerItem(bag, slot)
+              else
+                SplitContainerItem(bag, slot, difference)
+              end
+
+              PutSplitItemIntoBags(item, difference)
+
+              itemsInBags[item.itemName] = itemsInBags[item.itemName] and itemsInBags[item.itemName] + difference or difference
+              hasSplitItems = true
+              --coroutine.yield()
+            end
+          end
+        end -- if item we should get and its not locked
+      end -- for slot
+    end -- for bag
+  end -- not right clicked an item
+
+
+  -- 
+  if rightClickedItem == false and transferredToBank == false and hasSplitItems == false then
+    core.currentlyRestocking = false
+    core:Print(core.defaults.prefix .. "finished restocking from bank.")
+  end
+end
+
+
+restockerCoroutine = coroutine.create(bankTransfer)
+
+
+--
+-- OnUpdate frame
+--
+
+local onUpdateFrame = CreateFrame("Frame")
+local ONUPDATE_INTERVAL = 0.05
+local timer = 0
+
+onUpdateFrame:SetScript("OnUpdate", function(self, elapsed)
+  timer = timer+elapsed
+  if core.currentlyRestocking then
+    --if timer >= ONUPDATE_INTERVAL then
+      timer = 0
+      if somethingLocked() and not CursorHasItem() then return end
+      if coroutine.status(restockerCoroutine) == "running" then return end
+
+      local resume = coroutine.resume(restockerCoroutine)
+      if resume == false then
+        restockerCoroutine = coroutine.create(bankTransfer)
+      end
+    --end
+
+    --[[
+
       if timer >= ONUPDATE_INTERVAL then
         timer = 0
         if coroutine.resume(restockerCoroutine) == false then
@@ -235,9 +297,9 @@
         end
         --bankTransfer()
       end
-    end
-  end)
-]]
+    ]]
+  end
+end)
 
 
 
@@ -245,6 +307,7 @@
 
 
 
+--[[
 local _, core = ...;
 
 core.didBankStuff = false
@@ -396,3 +459,5 @@ function core:pickupItem()
   end
   core.didBankStuff = false
 end
+
+]]
